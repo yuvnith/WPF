7195c63016c5aa3767419b73ab232a951c7dd915
vamshi <yuvnith.vamshi@gmail.com>
2018-05-10 13:44:45 +0530

    changing machine commit

---
+++ source/repos/WpfPractise/WpfPractise/Async.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfPractise
+{
+    /// <summary>
+    /// Interaction logic for Async.xaml
+    /// </summary>
+    public partial class Async : Window
+    {
+        ObservableCollection<Aa> col = new ObservableCollection<Aa>();
+        public Async()
+        {
+            InitializeComponent();
+            Gd1.DataContext = col;
+            gd1();
+        }
+
+
+        public async Task gd1()
+        {
+           
+
+
+            await Task.Run(() =>
+            {
+                for (var i = 0; i < 10000000; i++)
+                {
+                    col.Add(new Aa()
+                    {
+                        no = i
+                    });
+                }
+            });
+
+        }
+    }
+
+    public class Aa
+    {
+        public int no {
+            get;
+            set;
+        }
+    }
+}
